e962fbdbd82a7fb6cb4ccc5c9c62ea2092c9814e
michaelmedellin <13091665+michaelmedellin@users.noreply.github.com>
2024-08-24 11:54:55 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,15 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GRDBQuery",
-            dependencies: [
-                .product(name: "GRDB", package: "GRDB.swift"),
-            ]),
         .testTarget(
             name: "GRDBQueryTests",
-            dependencies: [
-                "GRDBQuery",
-                .product(name: "GRDB", package: "GRDB.swift"),
-            ]),
     ]
 )
 
